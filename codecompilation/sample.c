af1ea308e21f6dc61c68d7385352f5c65bb5ec3d
Bhuvana.M <bhuvanameenakshinathan@gmail.com>
2023-08-12 13:45:06 +0530

    code compilation steps

---
+++ codecompilation/sample.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("Sample program of code compilation - ");
+    int a=10;
+    int b=10;
+    printf("%d",a+b);
+
+}
---
+++ codecompilation/sample.s
@@ -0,0 +1,41 @@
+	.file	"sample.c"
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+	.align 4
+LC0:
+	.ascii "Sample program of code compilation - \0"
+LC1:
+	.ascii "%d\0"
+	.text
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB10:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$32, %esp
+	call	___main
+	movl	$LC0, (%esp)
+	call	_printf
+	movl	$10, 28(%esp)
+	movl	$10, 24(%esp)
+	movl	28(%esp), %edx
+	movl	24(%esp), %eax
+	addl	%edx, %eax
+	movl	%eax, 4(%esp)
+	movl	$LC1, (%esp)
+	call	_printf
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE10:
+	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
+	.def	_printf;	.scl	2;	.type	32;	.endef
